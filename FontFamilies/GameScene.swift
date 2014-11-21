39ff5bb7e418d05b4787963172af0d34eda53f52
Romaniuk Sergey <ukrduino@gmail.com>
2014-11-21 11:28:35 +0200

    Перелистывалка списка шрифтов в  IOS8

--- FontFamilies/GameScene.swift
+++ FontFamilies/GameScene.swift
@@ -6,37 +6,56 @@
 //  Copyright (c) 2014 Romaniuk Sergey. All rights reserved.
 //
 
+import UIKit
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+
+    var familyIdx: Int = 0
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
+    override init(size: CGSize) {
+            super.init(size: size)
+            showCurrentFamily()
+    }
+    
+    func showCurrentFamily() {
+        // 1
+        removeAllChildren()
+        // 2
+        let familyName = UIFont.familyNames()[familyIdx] as String
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        println("Family: \(familyName)  is \(familyIdx) from \(UIFont.familyNames().count)")
+        
+        // 3
+        let fontNames =
+        UIFont.fontNamesForFamilyName(familyName) as [String]
+        // 4
+        for (idx, fontName) in enumerate(fontNames) {
+            let label = SKLabelNode(fontNamed: fontName)
+            label.text = fontName
+            label.position = CGPoint(
+            x: size.width / 2,
+            y: (size.height * (CGFloat(idx+1))) /
+            (CGFloat(fontNames.count)+1))
+            label.fontSize = 25
+            label.verticalAlignmentMode = .Center
+            addChild(label)
+        
+        
         }
+    }
+    
+    override func touchesBegan(touches: NSSet,
+        withEvent event: UIEvent) {
+        familyIdx++;
+        if familyIdx >= UIFont.familyNames().count {
+        familyIdx = 0
+        }
+        showCurrentFamily()
     }
    
     override func update(currentTime: CFTimeInterval) {
